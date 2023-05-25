200510fb84dd96aa25bc638b0f98facfe03919f2
Adeyemi Oluwagbogo <israeladeyemi489@gmail.com>
2023-05-25 19:50:25 +0100

    contract

--- Contract/deploy/99-update-frontend.js
+++ Contract/deploy/99-update-frontend.js
@@ -1,34 +1,21 @@
 const { ethers, network } = require("hardhat");
-// const {  } = require("fs");
 const fs = require("fs-extra");
 const {
   FRONT_END_ABI_FILE,
   FRONT_END_ADDRESSES_FILE,
 } = require("../helper-hardhat-config");
 
-// const FRONT_END_ADDRESSES_FILE = "../../Client/src/utils/address.json";
-// const FRONT_END_ABI_FILE = "../../Client/src/utils/abi.json";
-
 module.exports = async () => {
   if (true) {
     console.log("updating frontend....");
-    // updateContractAddress();
+    updateContractAddress();
     // updateAbi();
-    console.log("updating frontend....");
+    console.log("updated frontend....");
   }
-  // const { ethers } = require("hardhat");
-  // console.log("started");
-  // const wallet = ethers.Wallet.createRandom();
-  // const address = wallet.address;
-  // console.log(wallet);
-  // console.log(address);
-  // const provider = ethers.getDefaultProvider('ropsten')
-  // console.log(provider)
 };
 async function updateAbi() {
   console.log("abi wroting");
   const guess2Win = await ethers.getContract("Guess");
-  console.log(`guess2Win: ${guess2Win}`);
   fs.writeFileSync(
     FRONT_END_ABI_FILE,
     guess2Win.interface.format(ethers.utils.FormatTypes.json)
@@ -38,14 +25,11 @@ async function updateAbi() {
 async function updateContractAddress() {
   console.log("address wroting");
   const guess2Win = await ethers.getContract("Guess");
-  console.log(guess2Win.address);
   const chainId = network.config.chainId.toString();
-  console.log(chainId);
-  const currentAddresses = JSON.parse(
+  const result = (
     fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf8")
   );
-  console.log(currentAddresses);
-  console.log(guess2Win.address);
+  const currentAddresses = JSON.stringify(result)
   if (chainId in currentAddresses) {
     if (!currentAddresses[chainId].includes(guess2Win.address)) {
       currentAddresses[chainId].push(guess2Win.address);
